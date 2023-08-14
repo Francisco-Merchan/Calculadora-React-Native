f5eb43526cf38ff6a71c65fbd1ec0f92b4bb1c2b
Francisco Merchan <merchanf6@gmail.com>
2023-08-14 16:54:27 -0300

    calculadora react-native

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {CalculadoraScreen} from './src/screens/CalculadoraScreen';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {styles} from './src/theme/appTheme';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar barStyle="light-content" backgroundColor="black" />
+      <CalculadoraScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
